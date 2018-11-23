40f149871767c99178c921ecddfe26b1b3d5ec2b
Miya Fordah <42814756+mafordah@users.noreply.github.com>
2018-11-22 20:08:10 -0700

    Final Display Test
    checking if it works in browser

--- Final/sketch.js
+++ Final/sketch.js
@@ -1,10 +1,15 @@
+let angler;
 
 function preload() {
-    img = loadImage('./data/background.png');
+    ocean = loadImage('./data/background.png');
+    ground = loadImage('./data/ground.png');
+    depth = loadImage('./data/depth.png');
+    anglerSprite = loadImage('./data/anglerSprite.png')
 }
 
 function setup(){
   createCanvas(windowWidth, windowHeight);
+  angler = new Angler();
 }
 
 function windowResized() {
@@ -12,14 +17,72 @@ function windowResized() {
 }
 
 function draw(){
-  background(img);
-  noCursor();
+  background(ocean);
+//  noCursor();
   scenary();
+  angler.frame();
+
 }
 
 function scenary(){
   //ground
-  fill('rgb(46, 41, 37)');
-  noStroke();
-  rect(0, windowHeight * 3/4, windowWidth, windowHeight/4);
+  image(ground, 0, 0, windowWidth, windowHeight);
+  //ocean depth
+  image(depth, 0, 0, windowWidth, windowHeight);
+}
+
+class Angler{
+  contructor(){
+    this.xPos = 500;
+    this.yPos = 400;
+    this.image = anglerSprite;
+    this.speed = 4;
+    this.width = 672;
+    this.height = 480;
+    this.spriteFrame = 0
+    this.subRect = [
+      [0, 0],
+      [672, 0],
+      [0, 480],
+      [672, 480],
+      [0, 960],
+      [672, 960],
+    ];
+  }
+
+  frame(){
+    this.display();
+    this.animate();
+  }
+
+  display(){
+    push();
+      translate(this.xPos, this.yPos);
+
+      image(
+        this.image,
+        0,
+        0,
+        this.width,
+        this.height,
+        this.subRect[this.spriteFrame][0],
+        this.subrect[this.spriteFrame][1],
+        this.width,
+        this.height,
+      );
+
+    pop();
+  }
+
+  move(){
+
+  }
+
+  animate(){
+    if (frameCount % this.speed === 0) {
+      this.spriteFrame++;
+      this.spriteFrame %= 8;
+    }
+  }
+
 }
