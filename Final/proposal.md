22dafc7082ddb93ecc02fb09ee42291d34a28ea0
Miya Fordah <42814756+mafordah@users.noreply.github.com>
2018-11-28 16:19:43 -0700

    Proposal

---
+++ Final/proposal.md
@@ -0,0 +1,18 @@
+Miya Fordah
+
+# Final Project Proposal
+
+For my final project I'm creating a simple game concept stemming from the angler fish idea I had for homework 10. You are an angler fish controlled by the mouse that can eat the other fish that come across the screen. I have ideas about different fish that go faster, or ones that you can't eat, but I figure I should work my way up to that and see what I have time for. I plan to create a health bar that slowly depletes if you don't eat enough fish in time so there are some stakes to the game.
+
+My first inspiration, other than my own work this semester, was an old online game called Big Fish. You start out as a smaller fish and your fish slowly gets bigger as it eats more and more smaller fish until eventually you can pretty much eat everything on the screen. That's not exactly what I'm going for, but it's what I was thinking of in the game mechanics. Conceptually, I was definitely thinking of Finding Nemo when I decided on the angler fish. The drawings are kind of my own style but they came from actual pictures of an angler and a small bioluminescent fish. I also looked into the code of the student example games to get an idea of what is possible as far as simple menus and sprites.
+
+I know I'm going to have to figure out how to make a menu, or at least a way to start the game paused. I did make a menu a few days ago but, because my objects are called in the set up, the bubbles and fish don't stop calling and hundreds of objects move at once when the menu goes away. I also want to create a scrolling background, but that's on the back burner until I can get the base game running. Lastly I have no idea how to approach the health bar yet. Because I want it to run at all screen sizes, I have to find a way for it to work and still work when the screen is resized.
+
+I've been working on this project nearly everyday since week 13. I have a pretty good idea of what I plan on getting done:
+
+* November 29th: Finish fish eating mechanic
+* November 30th: Draw fish eating animation and apply it to angler fish class
+* December 1st - 2nd: Finish health bar
+* December 3rd - due date: Add as much scenery detail, extra functions, and animations as possible
+
+So far I've made the sprites and the movement functions for the angler fish and the main prey fish. It's one of my first times animating things in 2D so I only did a small amount of frames for each one. They look silly and cartoony like I wanted. I also added sets of bubbles for depth and movement to the scene. I've been working on background objects and have a sand texture that I hope to have scrolling later. Even though I don't have very many of the game elements in place, it's still an interesting scene to mess with so far.
--- Final/sketch.js
+++ Final/sketch.js
@@ -1,88 +1,377 @@
-let angler;
+let angler = [];
+let backBubbles = [];
+let frontBubbles = [];
+let fish = [];
+
+
 
 function preload() {
-    ocean = loadImage('./data/background.png');
-    ground = loadImage('./data/ground.png');
-    depth = loadImage('./data/depth.png');
-    anglerSprite = loadImage('./data/anglerSprite.png')
+  //preload angler sprite
+    anglerSprite = loadImage('./data/anglerSprite.png');
+    fishSprite = loadImage('./data/fishSprite.png');
+    sand = loadImage('./data/sand.png');
 }
+
+
 
 function setup(){
   createCanvas(windowWidth, windowHeight);
-  angler = new Angler();
+
+
+  //limit fish
+    fishTimer();
+
+
+
+  //define angler
+    angler[0] = new Angler();
+
+
+  //limit bubbles
+    frontBubbleTimer();
+    backBubbleTimer();
 }
+
+
 
 function windowResized() {
-  resizeCanvas(windowWidth, windowHeight);
+  //cavas resize with window
+    resizeCanvas(windowWidth, windowHeight);
 }
+
+
 
 function draw(){
-  background(ocean);
-//  noCursor();
-  scenary();
-  angler.frame();
+    gameScreen();
+}
+
+
+
+function mousePressed(){
 
 }
 
-function scenary(){
-  //ground
-  image(ground, 0, 0, windowWidth, windowHeight);
-  //ocean depth
-  image(depth, 0, 0, windowWidth, windowHeight);
+
+
+function startScreen(){
+  background('rgb(246, 179, 252)');
+  noCursor();
+  fill('rgb(24, 14, 51)')
+  textAlign(CENTER);
+  text('Click To Start', width/2, height/2);
 }
+
+
+
+function gameScreen(){
+  background('rgb(24, 14, 51)');
+  noCursor();
+
+
+  //back bubbles
+    //create back bubbles
+    for(let i = 0; i < backBubbles.length; i++){
+      backBubbles[i].move();
+      backBubbles[i].draw();
+    }
+
+    //if back bubble hits edge, the bubble will splice
+    for(let i = backBubbles.length - 1; i >= 0; i--){
+      let pop = backBubbles[i].edgeCheck();
+      if(pop){
+        backBubbles.splice(i, 1);
+      }
+    }
+
+
+  //draw scenery
+    scenery();
+
+
+  //draw fish
+    for(let i = 0; i < fish.length; i++){
+      fish[i].frame();
+    }
+
+
+  //draw angler
+    for(let i = 0; i < 1; i++){
+      angler[i].frame();
+    }
+
+
+  //front bubbles
+    //create front bubbles
+      for(let i = 0; i < frontBubbles.length; i++){
+        frontBubbles[i].move();
+        frontBubbles[i].draw();
+      }
+
+    //if front bubble hits edge, the bubble will splice
+      for(let i = frontBubbles.length - 1; i >= 0; i--){
+        let pop = frontBubbles[i].edgeCheck();
+        if(pop){
+          frontBubbles.splice(i, 1);
+        }
+      }
+
+  //fish end
+    for(let i = fish.length - 1; i >= 0; i--){
+      let end = fish[i].edgeCheck();
+      if (end) {
+        fish.splice(i, 1);
+      }
+    }
+
+  //front ground
+    fill('rgb(177, 173, 144)');
+    noStroke();
+    rect(0, height * 19/20, width, height/20);
+    //sand
+    image(sand, 0, windowHeight * 1/5, windowWidth, windowHeight * 9/10);
+
+
+}
+
+
+
+function scenery(){
+  //back ground
+    fill('rgb(177, 173, 144)');
+    noStroke();
+    rect(0, height * 4/5, width, height/5);
+    //sand
+    image(sand, 0, 0, windowWidth, windowHeight);
+}
+
+
+
+function backBubbleTimer(){
+  let bb = new Bubble(random(width + 300), height * 4/5 + 20, random(5, 15));
+  backBubbles.push(bb);
+  setTimeout(backBubbleTimer, 25);
+}
+
+
+
+function frontBubbleTimer(){
+  let fb = new Bubble(random(width + 300), height, random(15, 30));
+  frontBubbles.push(fb);
+  setTimeout(frontBubbleTimer, 100);
+}
+
+
+
+function fishTimer(){
+  /*for(let i = 0; i < 15; i++){
+    fish.push(new Fish(width, random(height/20, height * 17/20)));
+  }*/
+  let f = new Fish(width, random(height/20, height * 4/5));
+  fish.push(f);
+  setTimeout(fishTimer, random(1000, 2000));
+}
+
+
 
 class Angler{
-  contructor(){
-    this.xPos = 500;
-    this.yPos = 400;
+  constructor(){
+    //call sprite sheet
     this.image = anglerSprite;
-    this.speed = 4;
-    this.width = 672;
-    this.height = 480;
+    //set frame speed
+    this.speed = 6;
+    //define sprite size
+    this.size = {
+      w: 504,
+      h: 360
+    };
+    //define current sprite frame
     this.spriteFrame = 0
     this.subRect = [
       [0, 0],
-      [672, 0],
-      [0, 480],
-      [672, 480],
-      [0, 960],
-      [672, 960],
+      [504, 0],
+      [0, 360],
+      [504, 360],
+      [0, 720],
+      [504, 720],
     ];
+    //define movement variables
+    this.moveX;
+    this.moveY;
+    this.movementX;
+    this.movementY;
+    this.x = windowWidth/7;
+    this.y = windowHeight/5;
+    this.follow = 0.05;
   }
 
+
   frame(){
+    //call methods in one method
     this.display();
     this.animate();
+    this.move();
+  }
+
+  move(){
+    this.movementX = mouseX - 100;
+    this.moveX = this.movementX - this.x;
+    this.x += this.moveX * this.follow;
+
+
+    this.movementY = mouseY - 240;
+    this.moveY = this.movementY - this.y;
+    this.y += this.moveY * this.follow;
   }
 
   display(){
     push();
-      translate(this.xPos, this.yPos);
+      //move with mouse at center of angler fish
+      translate(this.x, this.y);
+
 
       image(
-        this.image,
-        0,
-        0,
-        this.width,
-        this.height,
-        this.subRect[this.spriteFrame][0],
-        this.subrect[this.spriteFrame][1],
-        this.width,
-        this.height,
+          //call sprite sheet
+          this.image,
+          //location is translate
+          0,
+          0,
+          //image size
+          this.size.w,
+          this.size.h,
+          //define intial subRect postion
+          this.subRect[this.spriteFrame][0],
+          this.subRect[this.spriteFrame][1],
+          //subRect size
+          this.size.w,
+          this.size.h
       );
-
     pop();
   }
 
-  move(){
-
-  }
 
   animate(){
+    // change subRect postion over time
     if (frameCount % this.speed === 0) {
       this.spriteFrame++;
-      this.spriteFrame %= 8;
+      this.spriteFrame %= 6;
+    }
+  }
+}
+
+
+
+class Bubble{
+  constructor(x, y, d){
+    this.x = x;
+    this.y = y;
+    this.d = d;
+    this.fill = color('rgba(162, 242, 249, 0.2)');
+  }
+
+
+  move(){
+    //smaller bubbles move slower to create depth
+    this.x -= this.d/3
+    this.y -= this.d/3
+  }
+
+
+  draw(){
+    //draw bubble
+      noStroke();
+      fill(this.fill);
+      ellipse(this.x, this.y, this.d);
+  }
+
+
+  edgeCheck(){
+    //pop bubble if it reaches the edge of the screen
+    if (this.x + this.d < 0 || this.y + this.d < 0){
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
+
+
+
+class Fish{
+  constructor(x, y){
+    this.x = x;
+    this.y = y;
+    //call sprite sheet
+    this.image = fishSprite;
+    //set frame speed
+    this.speed = 3;
+    //define sprite size
+    this.size = {
+      w: 113,
+      h: 72
+    };
+    //define current sprite frame
+    this.spriteFrame = 0
+    this.subRect = [
+      [0, 0],
+      [216, 0],
+      [0, 144],
+      [216, 144],
+    ];
+  }
+
+
+  frame(){
+    this.move();
+    this.display();
+    this.animate();
+  }
+
+  move(){
+    this.x -= random(1, 7);
+    //this.x -=;
+  }
+
+
+  display(){
+    push();
+      //move with mouse at center of angler fish
+      translate(this.x, this.y);
+
+
+      image(
+          //call sprite sheet
+          this.image,
+          //location is translate
+          0,
+          0,
+          //image size
+          this.size.w,
+          this.size.h,
+          //define intial subRect postion
+          this.subRect[this.spriteFrame][0],
+          this.subRect[this.spriteFrame][1],
+          //subRect size
+          226,
+          144
+      );
+    pop();
+  }
+
+
+  animate(){
+    // change subRect postion over time
+    if (frameCount % this.speed === 0) {
+      this.spriteFrame++;
+      this.spriteFrame %= 4;
     }
   }
 
+
+  edgeCheck(){
+    if (this.x + 226 < 0){
+      return true;
+    } else {
+      return false;
+    }
+  }
 }
